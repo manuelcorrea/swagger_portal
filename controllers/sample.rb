ed305b51c0e87b5fa786b5202c0b6cff8f40e25d
mcorrea <manuel.correa@careerbuilder.com>
2015-05-22 15:29:27 -0400

    showing swagger

--- controllers/sample.rb
+++ controllers/sample.rb
@@ -1,3 +1,25 @@
 get '/sample' do
   'Hello world'
 end
+
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/viewer' do
+  erb :viewer, :locals => {:document => params[:document]}
+end
+
+get '/document/:document' do
+  send_file File.join(settings.public_folder, "swagger_docs/#{params['document']}.json")
+end
+
+
+get '/apis' do
+  @swagger_docs = []
+  Dir[File.join(File.dirname(__FILE__), '../public/swagger_docs/', '*.json')].each {
+      |file| @swagger_docs.push( File.basename(file, '.json' ) )
+  }
+  puts @swagger_docs
+  erb :apis
+end
